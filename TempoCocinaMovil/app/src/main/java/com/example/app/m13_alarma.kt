7c413dbbfb31cb611500acb50c07cd079255b99b
David Rojas J. <d.rojasj2@uniandes.edu.co>
2026-09-23 07:42:20 -0500

    🎨 (Design) adicion iconos a botones M-01, M-08 y M-13

--- TempoCocinaMovil/app/src/main/java/com/example/app/m13_alarma.kt
+++ TempoCocinaMovil/app/src/main/java/com/example/app/m13_alarma.kt
@@ -2,7 +2,7 @@ package com.example.app
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,13 +13,15 @@ class m13_alarma : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_m13_alarma)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnDetener = findViewById<Button>(R.id.btnDetener)
+        // Botón Detener (LinearLayout con ic_detener)
+        val btnDetener = findViewById<LinearLayout>(R.id.btnDetener)
         btnDetener.setOnClickListener {
             val intent = Intent(this, m07_proceso_terminado::class.java)
             startActivity(intent)
